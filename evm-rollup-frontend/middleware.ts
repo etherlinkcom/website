fe35f5d76772a58e837b105a65f7cadff0d91e2f
rickykuo666 <ricky.kuo@trili.tech>
2023-07-07 11:20:23 +0100

    test

--- evm-rollup-frontend/middleware.ts
+++ evm-rollup-frontend/middleware.ts
@@ -1,10 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const isPasswordEnabled = !!process.env.PASSWORD_PROTECT;
+
+console.log("isPasswordEnabled", isPasswordEnabled);
+
 export async function middleware(req: NextRequest) {
   const isLoggedIn = req.cookies.has("login");
+
+  console.log("isLoggedIn", isLoggedIn);
+
   const isPathPasswordProtect =
     req.nextUrl.pathname.startsWith("/password-protect");
+
+  console.log("isPathPasswordProtect", isPathPasswordProtect);
 
   if (isPasswordEnabled && !isLoggedIn && !isPathPasswordProtect) {
     return NextResponse.redirect(new URL("/password-protect", req.url));
