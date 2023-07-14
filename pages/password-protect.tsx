023bda83c0fad3f34591bac33f9762cd7747effb
rickykuo666 <ricky.kuo@trili.tech>
2023-07-14 16:26:14 +0100

    password bg

--- pages/password-protect.tsx
+++ pages/password-protect.tsx
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import Router from "next/router";
-import { Box, Flex, Button, Center, Text } from "@chakra-ui/react";
-import PasswordInput from "@/components/PasswordInput";
+import React, { useState } from 'react'
+import Router from 'next/router'
+import { Box, Flex, Button, Center, Text } from '@chakra-ui/react'
+import PasswordInput from '@/components/PasswordInput'
 
 const PasswordProtectPage = () => {
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
   const submit = async () => {
-    setError("");
+    setError('')
 
-    const res = await fetch("/api/password-protect", {
+    const res = await fetch('/api/password-protect', {
       body: JSON.stringify({
-        password,
+        password
       }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      method: "POST",
-    });
+      method: 'POST'
+    })
 
     if (res.status == 200) {
-      Router.push("/");
+      Router.push('/')
     } else {
-      setError("Wrong Passwrod");
+      setError('Wrong Passwrod')
     }
-  };
+  }
 
   return (
-    <Center mt="30%">
+    <Center bg='white' mt='30%'>
       <Box>
         <Flex>
           <PasswordInput password={password} setPassword={setPassword} />
           <Button
-            ml="10px"
-            colorScheme="messenger"
+            ml='10px'
+            colorScheme='messenger'
             onClick={submit}
             isDisabled={!password.length}
           >
@@ -42,12 +42,12 @@ const PasswordProtectPage = () => {
           </Button>
         </Flex>
         {error && (
-          <Text color="red" fontWeight={700}>
+          <Text color='red' fontWeight={700}>
             {error}
           </Text>
         )}
       </Box>
     </Center>
-  );
-};
-export default PasswordProtectPage;
+  )
+}
+export default PasswordProtectPage
