f5b919d0db81109113545abd49d0f7febd525c6b
sashaaldrick <sasha@aldrick.dev>
2023-10-23 18:58:15 +0100

    fix x link footer

--- components/footer.js
+++ components/footer.js
@@ -69,7 +69,7 @@ export default function Footer() {
             <div>Join the Community</div>
             <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
               <a
-                href="https://x.com/etherlink.com"
+                href="https://x.com/etherlinkcom"
                 target="_blank"
                 rel="noopener">
                 <span className="sr-only hover:fill-shaderGreen">X</span>
