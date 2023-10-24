5ffd8235b3bd161807c37d702f4ac631dc13a41b
sashaaldrick <sasha@aldrick.dev>
2023-10-24 18:03:33 +0100

    roadmap mvp

--- components/cta.js
+++ components/cta.js
@@ -4,18 +4,14 @@ import Container from "./container";
 const Cta = () => {
   return (
     <Container>
-      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-borderGreen px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl -mt-20">
+      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-borderGreen px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
-            Future features of Etherlink:
+            Ready to learn more about Etherlink?
           </h2>
-          <div className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
-            <ul className="list-disc">
-              <li className="ml-4 text-l">State-of-the-art upgrade process with governance</li>
-              <li className="ml-4 text-l">State-of-the-art upgrade process with governance</li>
-              <li className="ml-4 text-l">State-of-the-art upgrade process with governance</li>
-            </ul>
-          </div>
+          <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
+            Our step-by-step guides will help you get started.
+          </p>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
           <a
