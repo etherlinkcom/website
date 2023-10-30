89b58de66c4eddc62c62022f2cf7fbdcd8181078
sashaaldrick <sasha@aldrick.dev>
2023-10-30 19:31:02 +0000

    yolo into full functionality

--- components/faucet.js
+++ components/faucet.js
@@ -4,6 +4,7 @@ import {
   useConnectionStatus
 } from "@thirdweb-dev/react";
 
+
 // Thirdweb Connect Wallet Theme
 import { lightTheme } from "@thirdweb-dev/react";
 const customTheme = lightTheme({
@@ -16,21 +17,36 @@ const customTheme = lightTheme({
   },
 });
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image"
 import ReCAPTCHA from "react-google-recaptcha";
 
 const Faucet = () => {
-  
+
   const [captchaCompleted, setCaptchaCompleted] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [tokensClaimed, setTokensClaimed] = useState(false)
+  const [txHash, setTxHash] = useState("")
+
+  // useEffect(() => {
+  //   if (txHash) {
+  //     window.open(`https://explorer.ghostnet-evm.tzalpha.net/tx/${txHash}`, '_blank'); // replace with your URL
+  //   }
+  // }, [txHash]);
 
   function onCaptchaComplete() {
     console.log("In Captcha complete, completing innit...")
     setCaptchaCompleted(true)
   }
 
+  function parseReturnData(data) {
+    console.log(data.body.receipt.transactionHash);
+    setTxHash(data.body.receipt.transactionHash)
+  }
+
   const callFaucet = async () => {
     const body = JSON.stringify({ walletAddress: address });
+    setIsLoading(true);
     const response = await fetch('/api/faucet', {
       method: 'POST',
       headers: {
@@ -42,6 +58,7 @@ const Faucet = () => {
     if (response.ok) {
       const data = await response.json();
       console.log(data);
+      parseReturnData(data);
       setTokensClaimed(true);
     } else {
       console.error('Error:', response.status);
@@ -70,12 +87,24 @@ const Faucet = () => {
           />
         }
       </div>
-      {walletStatus === "connected" && captchaCompleted == true && tokensClaimed == false ?
+      {walletStatus === "connected" && captchaCompleted == true ?
         <button
-          onClick={(callFaucet)}
-          className="inline-block py-3 ml-4 text-lg font-medium text-center text-black border-solid border-2 border-black rounded-md px-7 lg:px-6 lg:py-4 hover:bg-borderGreen hover:border-borderGreen hover:text-black"
+          onClick={txHash ? () => window.open(`https://explorer.ghostnet-evm.tzalpha.net/tx/${txHash}`, '_blank') : callFaucet}
+          disabled={isLoading}
+          className={`inline-block py-3 ml-4 text-lg font-medium text-center text-black border-solid border-2 border-black rounded-md px-7 lg:px-6 lg:py-4 hover:bg-borderGreen hover:border-borderGreen hover:text-black ${isLoading ? 'opacity-50 hover:bg-white hover:border-black cursor: not-allowed' : ''}`}
         >
-          Send 1 XTZ to {shortAddress}
+          {isLoading ? 'Loading...' : txHash ?
+            <>
+              <Image
+                src="/img/etherlinkLogo.png"
+                alt="N"
+                width="128"
+                height="128"
+                className="w-8 mr-2"
+              />
+              {`${txHash.slice(0, 6)}...${txHash.slice(-4)}`}
+            </> :
+            `Send 1 XTZ to ${shortAddress}`}
         </button> : ""
       }
     </div>
