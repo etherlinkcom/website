6257b18816fc6dd690e909ffe8f358da2065a2aa
Sasha Aldrick <sasha@aldrick.dev>
2024-01-23 20:33:42 +0000

    change fathom component layout

--- app/layout.js
+++ app/layout.js
@@ -2,22 +2,22 @@ import ClientLayout from "./ClientLayout"
 import FathomComponent from "./components/fathom";
 
 export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
-            rel="stylesheet"
-          />
-        </head>
-        <body>
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
         <ClientLayout>
           {children}
+          <FathomComponent />
         </ClientLayout>
-        <FathomComponent />
-        </body>
-      </html>
-    );
+      </body>
+    </html>
+  );
 }
 
 
