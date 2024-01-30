c45fd6d5aa78e8863856a7678a250fa386b8e2e2
Sasha Aldrick <sasha@aldrick.dev>
2024-01-30 13:52:00 +0000

    add intent link

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -118,7 +118,7 @@ export default function Navbar() {
     // { name: "🐣 Incubator 🐣", link: "/incubator" },
     { name: "Faucet", onClick: () => { setShowFaucet(!showFaucet); } },
     { name: "Explorer", link: "https://explorer.etherlink.com/" },
-    { name: "Bridge", link: "https://bridge.etherlink.com/" },
+    // { name: "Bridge", link: "https://bridge.etherlink.com/" },
     // { name: "Connect Wallet", component: <ConnectWalletButton />, condition: pathname === '/faucet' },
   ];
 
