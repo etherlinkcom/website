0d6a319eefcfeb7b7c2feaa62e717a24cbc69fb0
Sasha Aldrick <sasha@aldrick.dev>
2024-01-30 10:59:12 +0000

    added back explorer

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -117,7 +117,7 @@ export default function Navbar() {
   const navigation = [
     // { name: "🐣 Incubator 🐣", link: "/incubator" },
     { name: "Faucet", onClick: () => { setShowFaucet(!showFaucet); } },
-    { name: "Explorer", link: "https://testnet-explorer.etherlink.com/" },
+    { name: "Explorer", link: "https://explorer.etherlink.com/" },
     { name: "Bridge", link: "https://bridge.etherlink.com/" },
     // { name: "Connect Wallet", component: <ConnectWalletButton />, condition: pathname === '/faucet' },
   ];
