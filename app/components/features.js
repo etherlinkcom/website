9d02402bdda40cafc25be9a02c7b5fbbb301ad92
rickykuo666 <ricky.kuo@trili.tech>
2024-02-28 15:25:20 +0000

    update copy

--- app/components/features.js
+++ app/components/features.js
@@ -6,33 +6,42 @@ export default function Features() {
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3 -mb-4">
         <div className="lg:col-span-2 xl:col-auto">
-          <div className="flex flex-col justify-between w-full h-full px-14 rounded-2xl py-14 bg-trueGray-800 shadow-md shadow-slate-400/40 hover:shadow-lg hover:shadow-darkGreen">
-            <h2 className="text-2xl leading-normal text-darkGreen text-center">
+          <div className="flex flex-col w-full h-full p-10 rounded-2xl bg-trueGray-800 shadow-md shadow-slate-400/40 hover:shadow-lg hover:shadow-darkGreen">
+            <h2 className="text-2xl mb-6 leading-normal text-darkGreen text-center">
               Decentralized Sequencer
             </h2>
             <p className="text-2xl leading-normal text-center">
-              Etherlink's <span className="text-darkGreen">state-of-the-art</span> consensus mechanism provides best-in-class censorship resistance.
+              The <span className="text-darkGreen">decentralized</span>{" "}
+              sequencer reduces the risk of{" "}
+              <span className="text-darkGreen">centralised control</span> and
+              <span className="text-darkGreen">manipulation</span>.
             </p>
           </div>
         </div>
         <div className="">
-          <div className="flex flex-col justify-between w-full h-full px-14 rounded-2xl py-14 bg-trueGray-800 shadow-md shadow-slate-400/40 hover:shadow-lg hover:shadow-darkGreen">
-            <h2 className="text-2xl leading-normal text-darkGreen text-center">
-              Very Low Fees
+          <div className="flex flex-col w-full h-full p-10 rounded-2xl bg-trueGray-800 shadow-md shadow-slate-400/40 hover:shadow-lg hover:shadow-darkGreen">
+            <h2 className="text-2xl mb-6 leading-normal text-darkGreen text-center">
+              MEV Protection
             </h2>
             <p className="text-2xl leading-normal text-center">
-              Etherlink has super low fees. How low? Think <span className="text-darkGreen">$0.01</span> per transaction, not $20. {' '}
+              Every year users lose{" "}
+              <span className="text-darkGreen">millions</span> of dollars,
+              Etherlink solves that with{" "}
+              <span className="text-darkGreen">MEV protection</span>{" "}
+              out-of-the-box.
             </p>
             <div className="mb-4"></div>
           </div>
         </div>
         <div className="">
-          <div className="flex flex-col justify-between w-full h-full px-14 rounded-2xl py-14 bg-trueGray-800 shadow-md shadow-slate-400/40 hover:shadow-lg hover:shadow-darkGreen">
-            <h2 className="text-2xl leading-normal text-darkGreen mb-4 text-center">
-              MEV Protection
+          <div className="flex flex-col w-full h-full p-10 rounded-2xl bg-trueGray-800 shadow-md shadow-slate-400/40 hover:shadow-lg hover:shadow-darkGreen">
+            <h2 className="text-2xl mb-6 leading-normal text-darkGreen text-center">
+              Very Low Fees
             </h2>
             <p className="text-2xl leading-normal text-center">
-              Etherlink's Decentralized Sequencer ensures fair ordering of transactions for all users <span className="text-darkGreen">out-of-the-box.</span>
+              Etherlink has super low fees. How low? Think{" "}
+              <span className="text-darkGreen">$0.01</span> per transaction,{" "}
+              <span className="text-darkGreen">not</span> $20.
             </p>
           </div>
         </div>
--- app/components/main.js
+++ app/components/main.js
@@ -1,8 +1,6 @@
-"use client"
+"use client";
 
 import Container from "./container";
-import dynamic from "next/dynamic";
-import Image from "next/image";
 import Link from "next/link";
 
 const MainContent = () => {
@@ -16,54 +14,62 @@ const MainContent = () => {
           <div className="text-xl leading-normal text-left lg:text-xl xl:text-2xl text-gray-300">
             An EVM-compatible optimistic rollup with :
             <ul className="list-disc list-inside mt-3 mb-3">
-              <li className="mb-1.5"><span className="text-darkGreen">fair </span> transaction ordering</li>
-              <li className="mb-1.5"><span className="text-darkGreen">fast </span>transaction execution</li>
-              <li><span className="text-darkGreen">security</span> as a priority</li>
+              <li className="mb-1.5">
+                <span className="text-darkGreen">Decentralised</span> - by
+                design
+              </li>
+              <li className="mb-1.5">
+                <span className="text-darkGreen">Secure</span> - users are
+                protected from exploitation
+              </li>
+              <li>
+                <span className="text-darkGreen">Affordable</span> - think $0.01
+                per transaction, not $20
+              </li>
             </ul>
-            <div> powered by Tezos <Link className="text-darkGreen hover:text-lightGreen cursor-pointer" href="https://tezos.com/developers/smart-rollups/" target="_blank" rel="noopener noreferrer">Smart Rollup</Link> technology.</div>
+            <div>
+              {" "}
+              powered by Tezos{" "}
+              <Link
+                className="text-darkGreen hover:text-lightGreen cursor-pointer"
+                href="https://tezos.com/developers/smart-rollups/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Smart Rollup
+              </Link>{" "}
+              technology.
+            </div>
           </div>
         </div>
 
         <div className="flex flex-col justify-between sm:flex-row l:items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center">
-          <a
+          <Link
             href="https://docs.etherlink.com"
             target="_blank"
             rel="noopener"
-            className="inline-block py-3 w-full sm:w-1/2 text-lg font-medium text-center text-black bg-white border-solid border-2 border-white rounded-md px-7 lg:px-10 lg:py-4 hover:bg-darkGreen hover:border-darkGreen hover:text-black">
+            className="inline-block py-3 w-full sm:w-1/2 text-lg font-medium text-center text-black bg-white border-solid border-2 border-white rounded-md px-7 lg:px-10 lg:py-4 hover:bg-darkGreen hover:border-darkGreen hover:text-black"
+          >
             Start Building
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://x.com/intent/user?screen_name=etherlinkcom"
             target="_blank"
             rel="noopener"
-            className="inline-block py-3 w-full sm:w-1/2 text-lg font-medium text-center text-white border-solid border-2 border-white rounded-md px-7 lg:px-6 lg:py-4 hover:bg-darkGreen hover:border-darkGreen hover:text-black">
-            Follow Etherlink 
-          </a>
+            className="inline-block py-3 w-full sm:w-1/2 text-lg font-medium text-center text-white border-solid border-2 border-white rounded-md px-7 lg:px-6 lg:py-4 hover:bg-darkGreen hover:border-darkGreen hover:text-black"
+          >
+            Follow Etherlink
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default function Main() {
-
   return (
-    <>
-      <Container className="flex flex-wrap">
-        <div className="flex items-center justify-center w-full lg:w-1/2">
-          <Image
-            src="/img/home/main.png"
-            width="616"
-            height="617"
-            className={"object-cover"}
-            alt="Etherlink Pastel Green Blockchain Spiral"
-            loading="eager"
-          />
-        </div>
-        <MainContent />
-      </Container>
-    </>
+    <Container className="flex flex-wrap justify-center">
+      <MainContent />
+    </Container>
   );
 }
-
