d1318a39d5dcc356e91349497d24717b14181439
rickykuo666 <ricky.kuo@trili.tech>
2024-05-07 14:13:09 +0100

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/react'
 import FathomComponent from './components/fathom'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
   metadataBase: new URL('https://etherlink.com')
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='scroll-smooth'>
+      <GoogleTagManager gtmId='GTM-MQ8V746L' />
       <head>
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap'
