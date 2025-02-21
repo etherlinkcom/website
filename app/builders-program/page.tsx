b29a9a96a7e33195637b4c194956b95ff567bc74
Meng-Ju Kuo <138588184+rickykuo666@users.noreply.github.com>
2025-02-21 12:38:27 +0000

    Add program page (#111)
    ## Overview
    <!--- Description of the scope of the PR. Add details what has changed
    (and why if needed) --->
    This PR includes adding builders-program page
    ## Tickets
    <!--- Paste Asana/Linear tickets here --->
    
    https://linear.app/tezos/issue/ETH-11/etherlink-builders-program-page#comment-8e216104
    ## Notes for reviewer (optional)
    <!--- 
    - (For example): I wasn't sure whether to implement a caching strategy
    [here](https://…), so I left a comment and suggest that this is handled
    in a separate [ticket](https://…)
    --->

---
+++ app/builders-program/page.tsx
@@ -0,0 +1,39 @@
+import Container from '../components/container'
+import { PrimaryButton } from '../components/buttons/PrimaryButton'
+
+const Program = () => {
+  return (
+    <div>
+      <img
+        className='absolute top-0 w-[870px] h-[360px] md:w-[2320px] md:h-[960px] object-cover left-1/2 transform -translate-x-1/2'
+        src='/img/builders/etherlink-bg.svg'
+        alt='bg img'
+      />
+      <Container className='min-h-[calc(100vh-104px)] md:min-h-[calc(100vh-138px)] relative justify-center container px-8 py-24 mx-auto flex flex-col items-start'>
+        <div className='pb-16'>
+          <h1 className='text-[35px] md:text-[55px] md:text-5xl font-bold text-white tracking-[-0.7px] md:tracking-[-1.1px]'>
+            <span className='text-newGreen'>Etherlink</span>
+            <br /> Builders Program
+          </h1>
+          <p className='mt-4 md:text-lg text-[#9B9B9B]'>
+            Get mentorship from top experts, find co-founders and get funding to
+            accelerate your build.
+          </p>
+          <PrimaryButton
+            className='mt-8 text-lg'
+            text='Sign up today'
+            href='https://tt-tezos.typeform.com/bp-entry'
+            icon={
+              <img
+                src='/img/builders/arrow-up-right.svg'
+                alt='arrow right icon'
+              />
+            }
+          />
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Program
