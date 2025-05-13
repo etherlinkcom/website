446a1f8956b24b4989ec9c476ba0833cfa78ff88
rickykuo666 <ricky.kuo@trili.tech>
2025-05-13 13:06:17 +0100

    qa: update link

--- app/defi/OnBoard.tsx
+++ app/defi/OnBoard.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import Container from '../components/container'
+import Link from 'next/link'
 
 const ONBOARD_DATA = [
   {
     org: 'Gate.io',
     title: 'Centralized Exchange',
     description: 'Buy and transfer $XTZ to Etherlink',
-    link: '',
+    link: 'https://www.gate.io/',
     image: '/img/defi/stepCard.jpg'
   },
   {
     org: 'Tezos & Etherlink',
     title: 'Bridge',
     description: 'Bridge your assets to Etherlink',
-    link: '',
+    link: 'https://bridge.etherlink.com/',
     image: '/img/defi/stepCard.jpg'
   },
   {
     org: 'Transak',
     title: 'Onramp',
     description: 'Buy $XTZ using fiat',
-    link: '',
+    link: 'https://global.transak.com/',
     image: '/img/defi/stepCard.jpg'
   }
 ]
@@ -63,20 +64,22 @@ const OnBoardCard = ({
   image
 }: OnBoardCardProps) => {
   return (
-    <div className='border border-black-400 rounded-lg hover:border-neonGreen-500 group hover:cursor-pointer'>
-      <div className='rounded-lg'>
-        <img className='rounded-lg' src={image} alt='card' />
-      </div>
-      <div className='p-6 bg-grey-700 group-hover:bg-grey-500 rounded-lg'>
-        <div className='flex justify-between mb-1'>
-          <p className='text-grey-50 text-xs font-light uppercase'>{org}</p>
-          <img src='/img/defi/greenArrow.svg' alt='arrow' />
+    <Link href={link} target={link.includes('bridge') ? '_self' : '_blank'}>
+      <div className='border border-black-400 rounded-lg hover:border-neonGreen-500 group hover:cursor-pointer'>
+        <div className='rounded-lg'>
+          <img className='rounded-lg' src={image} alt='card' />
         </div>
-        <p className='text-2xl text-neonGreen-500 font-semibold mb-2'>
-          {title}
-        </p>
-        <p className='text-grey-200'>{description}</p>
+        <div className='p-6 bg-grey-700 group-hover:bg-grey-500 rounded-lg'>
+          <div className='flex justify-between mb-1'>
+            <p className='text-grey-50 text-xs font-light uppercase'>{org}</p>
+            <img src='/img/defi/greenArrow.svg' alt='arrow' />
+          </div>
+          <p className='text-2xl text-neonGreen-500 font-semibold mb-2'>
+            {title}
+          </p>
+          <p className='text-grey-200'>{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
