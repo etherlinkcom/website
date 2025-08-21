89b672e74ae1455830239dfa8bf067c43e69d1fb
rickykuo666 <ricky.kuo@trili.tech>
2025-08-21 15:13:05 +0100

    fix video style

--- app/defi/DesktopTutorialTable.tsx
+++ app/defi/DesktopTutorialTable.tsx
@@ -291,7 +291,7 @@ export const DesktopTutorialTable = ({
                     loop
                     muted
                     playsInline
-                    style={{ width: '100%', height: '100%' }}
+                    className='w-full h-full object-cover'
                   />
                 ) : (
                   <img
--- app/defi/MobileTutorialTable.tsx
+++ app/defi/MobileTutorialTable.tsx
@@ -258,7 +258,7 @@ export const MobileTutorialTable = ({
                   loop
                   muted
                   playsInline
-                  style={{ width: '100%', height: '100%' }}
+                  className='w-full h-full object-cover'
                 />
               ) : (
                 <img
