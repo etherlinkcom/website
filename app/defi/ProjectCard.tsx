e8fc79a4d8937f8decee929c00f5a8e49abe7216
rickykuo666 <ricky.kuo@trili.tech>
2025-09-18 16:52:57 +0100

    wip: style project card

--- app/defi/ProjectCard.tsx
+++ app/defi/ProjectCard.tsx
@@ -12,17 +12,21 @@ interface ProjectCardProps {
 export const ProjectCard = (props: ProjectCardProps) => {
   return (
     <Link href={props.link} passHref>
-      <div className='hover:cursor-pointer'>
-        <img src={props.image} alt={props.title} />
-        <div className='grey-700 p-6 border-l border-r border-b border-black-400 rounded-b-lg'>
+      <div className='hover:cursor-pointer flex flex-col h-full'>
+        <img
+          src={props.image}
+          alt={props.title}
+          className='w-full object-cover'
+        />
+        <div className='grey-700 p-6 border-l border-r border-b border-black-400 rounded-b-lg flex flex-col flex-grow'>
           <div className='flex items-center justify-between mb-1'>
             <h3 className='text-2xl font-semibold text-neonGreen-500'>
               {props.title}
             </h3>
             <img src='/img/defi/arrow-up-right-green.svg' alt='Arrow icon' />
           </div>
-          <p className='text-grey-200 mb-4'>{props.description}</p>
-          <span className='text-sm text-grey-50 px-2 py-1 rounded-full bg-grey-400'>
+          <p className='text-grey-200 mb-4 flex-grow'>{props.description}</p>
+          <span className='text-sm text-grey-50 px-2 py-1 rounded-full bg-grey-400 self-start'>
             {props.tag}
           </span>
         </div>
--- app/defi/ProjectSection.tsx
+++ app/defi/ProjectSection.tsx
@@ -16,7 +16,7 @@ export const ProjectSection = () => {
             Step-by-step guides to the top strategies on Etherlink
           </p>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch'>
           {PROJECTS.map(project => (
             <ProjectCard key={project.title} {...project} />
           ))}
