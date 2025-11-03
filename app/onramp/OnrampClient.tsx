42f22e849742ef005a68a04938d6da3afaabbc18
Alex Skene <510285+skenaja@users.noreply.github.com>
2025-11-03 12:22:18 +0000

    apply responsive styles to grid

--- app/onramp/OnrampClient.tsx
+++ app/onramp/OnrampClient.tsx
@@ -117,7 +117,7 @@ export const OnrampClient = () => {
             Transfer from a CEX, bridge assets, or use fiat
           </p>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {ONBOARD_DATA.map(item => (
             <OnBoardCard {...item} key={item.org} />
           ))}
